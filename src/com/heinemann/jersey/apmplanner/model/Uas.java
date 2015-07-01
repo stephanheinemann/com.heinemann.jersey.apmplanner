b509fb394c27beb2f0f8ae4ba0c8cc387313120f
Stephan Heinemann <stephan@rigi-lab-03.cs.uvic.ca>
2015-06-30 18:22:18 -0700

    added jersey APM planner

---
+++ src/com/heinemann/jersey/apmplanner/model/Uas.java
@@ -0,0 +1,115 @@
+package com.heinemann.jersey.apmplanner.model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Uas {
+	private int identifier;
+	private String name;
+	private String systemTypeName;
+	private String autopilotTypeName;
+	private String shortState;
+	private String shortMode;
+	private boolean isArmed;
+	private String batterySpecs;
+	private long uptime;
+	private double roll;
+	private double pitch;
+	private double yaw;
+
+	public int getIdentifier() {
+		return identifier;
+	}
+
+	public void setIdentifier(int identifier) {
+		this.identifier = identifier;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSystemTypeName() {
+		return systemTypeName;
+	}
+
+	public void setSystemTypeName(String systemTypeName) {
+		this.systemTypeName = systemTypeName;
+	}
+
+	public String getAutopilotTypeName() {
+		return autopilotTypeName;
+	}
+
+	public void setAutopilotTypeName(String autopilotTypeName) {
+		this.autopilotTypeName = autopilotTypeName;
+	}
+
+	public String getShortState() {
+		return shortState;
+	}
+
+	public void setShortState(String shortState) {
+		this.shortState = shortState;
+	}
+
+	public String getShortMode() {
+		return shortMode;
+	}
+
+	public void setShortMode(String shortMode) {
+		this.shortMode = shortMode;
+	}
+
+	public boolean isArmed() {
+		return isArmed;
+	}
+
+	public void setArmed(boolean isArmed) {
+		this.isArmed = isArmed;
+	}
+
+	public String getBatterySpecs() {
+		return batterySpecs;
+	}
+
+	public void setBatterySpecs(String batterySpecs) {
+		this.batterySpecs = batterySpecs;
+	}
+
+	public long getUptime() {
+		return uptime;
+	}
+
+	public void setUptime(long uptime) {
+		this.uptime = uptime;
+	}
+
+	public double getRoll() {
+		return roll;
+	}
+
+	public void setRoll(double roll) {
+		this.roll = roll;
+	}
+
+	public double getPitch() {
+		return pitch;
+	}
+
+	public void setPitch(double pitch) {
+		this.pitch = pitch;
+	}
+
+	public double getYaw() {
+		return yaw;
+	}
+
+	public void setYaw(double yaw) {
+		this.yaw = yaw;
+	}
+}
---
+++ src/com/heinemann/jersey/apmplanner/resource/UasResource.java
@@ -0,0 +1,56 @@
+package com.heinemann.jersey.apmplanner.resource;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.heinemann.grpc.apmplanner.ApmPlanner;
+import com.heinemann.grpc.apmplanner.UasManagerClient;
+import com.heinemann.jersey.apmplanner.model.Uas;
+
+@Path("/uas")
+public class UasResource {
+
+	@GET
+	@Produces({
+		MediaType.TEXT_PLAIN,
+		MediaType.TEXT_HTML,
+		MediaType.APPLICATION_XML,
+		MediaType.APPLICATION_XHTML_XML,
+		MediaType.APPLICATION_JSON })
+	public Uas getUas() {
+		UasManagerClient client = new UasManagerClient("rigi-lab-03.cs.uvic.ca", 50051);
+		int identifier = client.getActiveUas();
+		ApmPlanner.Uas clientUas = client.getUas(identifier);
+		
+		Uas uas = new Uas();
+		uas.setIdentifier(clientUas.getIdentifier());
+		uas.setName(clientUas.getName());
+		uas.setAutopilotTypeName(clientUas.getAutopilotTypeName());
+		uas.setShortState(clientUas.getShortState());
+		uas.setShortMode(clientUas.getShortMode());
+		uas.setArmed(clientUas.getIsArmed());
+		uas.setBatterySpecs(clientUas.getBatterySpecs());
+		uas.setUptime(clientUas.getUptime());
+		uas.setRoll(clientUas.getRoll());
+		uas.setPitch(clientUas.getPitch());
+		uas.setYaw(clientUas.getYaw());
+		
+		return uas;
+	}
+	
+	@PUT
+	public Response setMode() {
+		return null;
+	}
+	
+	@POST
+	public Response reboot() {
+		return null;
+	}
+
+}
