c32289d8313d6d8eab87f0048e67140b8e7af4d8
Stephan Heinemann <stephan@rigi-lab-03.cs.uvic.ca>
2015-07-04 21:09:44 -0700

    added event distribution service

---
+++ src/com/heinemann/jersey/apmplanner/events/EventDistributor.java
@@ -0,0 +1,53 @@
+package com.heinemann.jersey.apmplanner.events;
+
+import java.net.URI;
+import java.util.Iterator;
+
+import javax.ws.rs.core.Form;
+import javax.ws.rs.core.UriBuilder;
+
+import io.grpc.stub.StreamObserver;
+
+import com.heinemann.grpc.apmplanner.UasManagerClient;
+import com.heinemann.grpc.apmplanner.ApmPlanner.UasSubscriber;
+import com.heinemann.grpc.apmplanner.events.ApmEvents.Null;
+import com.heinemann.grpc.apmplanner.events.ApmEvents.UasEvent;
+import com.heinemann.grpc.apmplanner.events.UasEventDistributionGrpc;
+
+public class EventDistributor implements UasEventDistributionGrpc.UasEventDistribution {
+
+	public static final String IDENTIFIER = "identifier";
+	public static final String SOURCE = "source";
+	public static final String PARAMETERS = "parameters";
+	
+	@Override
+	public void fire(UasEvent request, StreamObserver<Null> responseObserver) {
+		distribute(request);
+		responseObserver.onValue(Null.newBuilder().build());
+		responseObserver.onCompleted();
+	}
+
+	private void distribute(UasEvent uasEvent) {
+		UasManagerClient client = new UasManagerClient(UasManagerClient.HOST, UasManagerClient.PORT);
+		Iterator<UasSubscriber> uasSubscribers = client.getSubscribers();
+		
+		while (uasSubscribers.hasNext()) {
+			UasSubscriber uasSubscriber = uasSubscribers.next();
+			URI subscriberUri = UriBuilder.fromUri(uasSubscriber.getSubscriber()).build();
+			
+			/*
+			ClientConfig config = new ClientConfig();
+			Client distributor = ClientBuilder.newClient(config);
+			WebTarget service = distributor.target(subscriberUri);
+			*/
+			
+			Form eventForm = new Form();
+			eventForm.param(IDENTIFIER, uasEvent.getIdentifier());
+			eventForm.param(SOURCE, uasEvent.getSource());
+			eventForm.param(PARAMETERS, uasEvent.getParameters());
+			//service.request().post(Entity.entity(eventForm, MediaType.APPLICATION_FORM_URLENCODED), Response.class);
+			System.out.println(subscriberUri);
+			System.out.println(eventForm);
+		}
+	}
+}
---
+++ src/com/heinemann/jersey/apmplanner/events/EventServer.java
@@ -0,0 +1,52 @@
+package com.heinemann.jersey.apmplanner.events;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import com.heinemann.grpc.apmplanner.events.UasEventDistributionGrpc;
+
+import io.grpc.ServerImpl;
+import io.grpc.transport.netty.NettyServerBuilder;
+
+public class EventServer implements ServletContextListener {
+
+	public static final String HOST = "rigi-lab-03.cs.uvic.ca";
+	public static final int PORT = 50052;
+	private ServerImpl server;
+
+	private void start() throws IOException {
+		server = NettyServerBuilder.forAddress(new InetSocketAddress(HOST, PORT))
+				.addService(UasEventDistributionGrpc.bindService(new EventDistributor())).build()
+				.start();
+		
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			@Override
+			public void run() {
+				EventServer.this.stop();
+			}
+		});
+	}
+
+	private void stop() {
+		if (server != null) {
+			server.shutdown();
+		}
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+		stop();
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+		try {
+			start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
