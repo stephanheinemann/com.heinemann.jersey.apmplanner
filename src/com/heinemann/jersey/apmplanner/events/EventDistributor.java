af19452ace499802fd23b6dc20baceb0f49af91f
Stephan Heinemann <stephan@rigi-lab-03.cs.uvic.ca>
2015-07-05 18:11:23 -0700

    improved event distribution

--- src/com/heinemann/jersey/apmplanner/events/EventDistributor.java
+++ src/com/heinemann/jersey/apmplanner/events/EventDistributor.java
@@ -3,13 +3,17 @@ package com.heinemann.jersey.apmplanner.events;
 import java.net.URI;
 import java.util.Iterator;
 
-import javax.ws.rs.core.Form;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
+import javax.xml.bind.JAXBElement;
+import javax.xml.namespace.QName;
 
 import org.glassfish.jersey.client.ClientConfig;
-import org.glassfish.jersey.client.JerseyClient;
-import org.glassfish.jersey.client.JerseyClientBuilder;
-import org.glassfish.jersey.client.JerseyWebTarget;
 
 import io.grpc.stub.StreamObserver;
 
@@ -20,10 +24,6 @@ import com.heinemann.grpc.apmplanner.events.ApmEvents.UasEvent;
 import com.heinemann.grpc.apmplanner.events.UasEventDistributionGrpc;
 
 public class EventDistributor implements UasEventDistributionGrpc.UasEventDistribution {
-
-	public static final String IDENTIFIER = "identifier";
-	public static final String SOURCE = "source";
-	public static final String PARAMETERS = "parameters";
 	
 	@Override
 	public void fire(UasEvent request, StreamObserver<Null> responseObserver) {
@@ -36,24 +36,18 @@ public class EventDistributor implements UasEventDistributionGrpc.UasEventDistri
 		UasManagerClient client = new UasManagerClient(UasManagerClient.HOST, UasManagerClient.PORT);
 		Iterator<UasSubscriber> uasSubscribers = client.getSubscribers();
 		
-		System.out.println("***** distributing event *****");
-		
 		while (uasSubscribers.hasNext()) {
 			UasSubscriber uasSubscriber = uasSubscribers.next();
 			URI subscriberUri = UriBuilder.fromUri(uasSubscriber.getSubscriber()).build();
 			
 			ClientConfig config = new ClientConfig();
-			JerseyClient distributor = (JerseyClient) JerseyClientBuilder.newClient(config);
-			JerseyWebTarget service = distributor.target(subscriberUri);
+			Client distributor = ClientBuilder.newClient(config);
+			WebTarget service = distributor.target(subscriberUri);
 			
-			Form eventForm = new Form();
-			eventForm.param(IDENTIFIER, uasEvent.getIdentifier());
-			eventForm.param(SOURCE, uasEvent.getSource());
-			eventForm.param(PARAMETERS, uasEvent.getParameters());
-			//service.request().post(Entity.entity(eventForm, MediaType.APPLICATION_FORM_URLENCODED), Response.class);
-			System.out.println("***** notification ****");
-			System.out.println(subscriberUri);
-			System.out.println(eventForm);
+			QName qname = new QName("http://www.heinemann.com", "uas-event");
+			JAXBElement<UasEvent> event = new JAXBElement<UasEvent>(qname, UasEvent.class, uasEvent);
+			//TODO: check if not collecting responses (or registering listeners) creates an issue
+			service.request().async().post(Entity.entity(event, MediaType.APPLICATION_XML), Response.class);
 		}
 	}
 }
