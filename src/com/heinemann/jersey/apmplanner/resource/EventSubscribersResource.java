65a2e56a341639184642729cdd72659e527b86d3
Stephan Heinemann <stephan.heinemann@hotmail.com>
2015-07-11 21:17:48 -0700

    corrected javascript include

--- src/com/heinemann/jersey/apmplanner/resource/EventSubscribersResource.java
+++ src/com/heinemann/jersey/apmplanner/resource/EventSubscribersResource.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -58,7 +58,7 @@ public class EventSubscribersResource {
 		return eventSubscribers;
 	}
 	
-	@POST
+	@PUT
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	@Produces(MediaType.TEXT_HTML)
 	public void addEventSubscriber(
